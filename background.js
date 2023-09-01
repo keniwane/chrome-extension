c388a35db5e0da06a4ada694965dfe47f36629b0
Ken <keniwane@gmail.com>
2023-08-31 18:53:05 -0700

    chrome

---
+++ background.js
@@ -0,0 +1,59 @@
+const FALLBACK_API = 'https://tosdr.org/api/v1/service/';
+
+async function getServiceDetails(domain) {
+    const response = await fetch(`${FALLBACK_API}all.json`);
+    if (response.ok) {
+        const responseData = await response.json();
+
+        // Remove "www." from the domain if present
+        const cleanDomain = domain.replace(/^www\./i, '');
+
+        // Check if the response contains the 'tosdr/review/' object for the domain
+        if (responseData[`tosdr/review/${cleanDomain}`]) {
+            const serviceDetails = responseData[`tosdr/review/${cleanDomain}`];
+            const serviceId = serviceDetails.id; // Extract the id
+
+            // Now you can make another API call using the service ID to get full details
+            const fullServiceDetails = await getToSDetails(serviceId);
+            return fullServiceDetails;
+        } else {
+            console.warn(`No service details found for ${domain}`);
+        }
+    } else {
+        console.error(`Failed to fetch all services. Status: ${response.status}`);
+    }
+    return null;
+}
+
+async function getToSDetails(serviceId) {
+    const response = await fetch(`${FALLBACK_API}${serviceId}.json`);
+    if (response.ok) {
+        const details = await response.json();
+        console.log("Full Service Details:", details);
+        return details;
+    } else {
+        console.error(`Failed to fetch service details. Status: ${response.status}`);
+        return null;
+    }
+}
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    if (message.action === 'getToSDetails' && message.domain) {
+        const domain = message.domain;
+
+        getServiceDetails(domain).then(serviceData => {
+            if (serviceData) {
+                sendResponse(serviceData);
+            } else {
+                sendResponse({ error: 'No service ID and details available' });
+            }
+        }).catch(error => {
+            console.error("Error in getServiceDetails:", error);
+            sendResponse({ error: 'Error fetching service details' });
+        });
+
+        return true; // indicates the response is sent asynchronously
+    } else {
+        sendResponse({ error: 'Invalid action or domain' });
+    }
+});
--- popup.js
+++ popup.js
@@ -1,15 +1,55 @@
-chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    let currentTab = tabs[0];
-    console.log(currentTab.url);
-    let url = currentTab.url
+// chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+//     const currentTab = tabs[0];
+//     const domain = new URL(currentTab.url).hostname;
+//     console.log("Sending message for tab:", currentTab, "Domain:", domain);
+
+//     // Send a message to the background script to get the service ID and ToS details
+//     chrome.runtime.sendMessage({ action: 'getToSDetails', domain: domain }, (response) => {
+//         if (response) {
+//             if (response.error) {
+//                 document.getElementById('rating').textContent = 'Rating: Not Available';
+//                 document.getElementById('summary').textContent = response.error;
+//             } else {
+//                 document.getElementById('rating').textContent = `Rating: ${response.class}`;
+//                 document.getElementById('summary').textContent = response.summary || 'No summary available.';
+//             }
+//         } else {
+//             document.getElementById('rating').textContent = 'Rating: Not Available';
+//             document.getElementById('summary').textContent = 'Failed to get a response.';
+//         }
+//     });
+// });
 
 
-    let test = document.createElement('div')
-    test.innerText = url
-    document.body.appendChild(test)
+// Listen for messages from the background script
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    // Display the TLDR if the message action is 'displayTLDR'
+    if (message.action === 'displayTLDR' && message.tldr) {
+        const tldrElement = document.getElementById('tldr'); // Replace with your element's ID
+        if (tldrElement) {
+            tldrElement.textContent = message.tldr;
+        }
+    }
+});
 
-})
+// Get the current active tab and its domain
+chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    const currentTab = tabs[0];
+    const domain = new URL(currentTab.url).hostname;
 
+    // Send a message to the background script to get the service details
+    chrome.runtime.sendMessage({ action: 'getToSDetails', domain: domain }, (response) => {
+        if (response && !response.error) {
+            const summary = response.pointsData[Object.keys(response.pointsData)[0]].tosdr.tldr;
+            // Update the rating and summary in the popup
+            document.getElementById('rating').textContent = `Rating: ${response.class}`;
+            document.getElementById('summary').textContent = summary || 'No summary available.';
 
-
-    // Use the URL to make your API call here
+            document.getElementById('loading').style.display = 'none';
+        } else {
+            document.getElementById('rating').textContent = 'Rating: Not Available';
+            document.getElementById('summary').textContent = 'Failed to get service details.';
+            document.getElementById('loading').style.display = 'none';
+        }
+    });
+});
